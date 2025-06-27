242342485a581935bc1eea99b092418f428849d0
sardorbek-3226 <ibrohimovs@gmail.com>
2025-06-27 18:30:15 +0300

    asdjh

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -36,7 +36,7 @@ const Home = ({ books, deleteBook }) => {
               <h3 className='text-2xl font-semibold pt-2'>{book.author}</h3>
               <h5 className='text-xl font-medium pt-2 pb-2'>{book.genre}</h5>
 
-              <div className='flex items-center container justify-between'>
+              <div className='flex items-center container justify-between flex-col-2'>
                 <div className='ml-3'>
                   <h2 className="line-through text-red-500 text-xl pr-10 mb-1">${book.price}</h2>
                   <h1 className="text-white/80 font-bold text-2xl pl-5 mb-1">${book.discountPrice}</h1>
